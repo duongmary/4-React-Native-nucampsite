8cc4b652085b53fbcbf7afcd5aef5285757fe361
Mary Duong <duongmary12@yahoo.com>
2020-10-16 20:57:45 -0400

    Navigator Part 3

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
-import { View, Platform  } from 'react-native';
+import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
@@ -11,7 +11,7 @@ const DirectoryNavigator = createStackNavigator(
     {
         Directory: { screen: Directory },
         CampsiteInfo: { screen: CampsiteInfo }
-    }, 
+    },
     {
         initialRouteName: 'Directory',
         defaultNavigationOptions: {
@@ -53,15 +53,14 @@ const MainNavigator = createDrawerNavigator(
     }
 );
 
-const AppNavigator = createAppContainer(MainNavigator);
+const AppNavigator = createAppContainer(MainNavigator)
 
 class Main extends Component {
     render() {
         return (
-            <View 
-                style={{
-                    flex: 1}}>
-                    paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
+            <View style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
             }}>
                 <AppNavigator />
             </View>
